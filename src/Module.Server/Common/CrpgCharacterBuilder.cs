553263260f4cc23b74131047bb11e69f8a560c96
vickscripts <nihrz421@gmail.com>
2025-06-06 00:37:49 -0500

    Squashed commit of the following:
    commit 828e8149a1760901e4f9517336eb0d0265c0b213
    Merge: 31b3f947 7e6d6c01
    Author: vickscripts <nihrz421@gmail.com>
    Date:   Fri Jun 6 00:31:52 2025 -0500
    
        Merge branch 'ChargeDamage2' of https://github.com/its-vick/crpg into ChargeDamage2
    
    commit 31b3f9472283458eade26a2aebf6c04099a9da00
    Author: vickscripts <nihrz421@gmail.com>
    Date:   Fri Jun 6 00:29:58 2025 -0500
    
        missed that
    
    commit 7e6d6c0168d8cbdea449a45cfa16a9a468a86eb4
    Merge: feab1aea aa71c773
    Author: vick <nihrz421@gmail.com>
    Date:   Fri Jun 6 00:13:04 2025 -0500
    
        Merge branch 'crpg2:master' into ChargeDamage2
    
    commit feab1aeab2abc2d3dfb90d03f9a1fc1507d769e7
    Author: vickscripts <nihrz421@gmail.com>
    Date:   Fri Jun 6 00:06:29 2025 -0500
    
        added more control and server configuration methods
    
        Removed ChargeDamageControl class and just checked rules inline
        Edited csproj files to exclude ChargeDamageCallback patch from editor and client
    
        Added more options in serverconfig to tweak how it works:
        crpg_charge_damage_disable_all False
        crpg_charge_damage_allow_enemies True
        crpg_charge_damage_allow_friendly True
        crpg_charge_damage_mirror_friendly_to_mount True
        crpg_charge_damage_mirror_friendly_to_agent False
        crpg_charge_damage_mirror_friendly_mount_damage_multiplier 4
        crpg_charge_damage_mirror_friendly_agent_damage_multiplier 0
        crpg_charge_damage_friendly_mount_max_damage 25
        crpg_charge_damage_friendly_mount_max_percentage 15
        crpg_charge_damage_min_velocity_for_friendly_damage 0.0
    
    commit 0fd3bf64162d9ef636354f6cb17b1e085ec6c912
    Author: vickscripts <nihrz421@gmail.com>
    Date:   Tue Jun 3 22:11:54 2025 -0500
    
        Added more control of charge damage rules and mirror damage options
    
        Now uses CrpgServerConfiguration variables/console commands to define/change its behavior.
    
        Added option to mirror damage to mount and damage multiplier
        Added option to mirror damage to agent and damage multiplier
    
        Changed harmony patch to a prefix to replace ChargeDamageCallback. Uses some reverse patched private methods in Mission.cs to replicate the original ChargeDamageCallback method with our added changes.
    
    commit f4d5a15790dce7ffbda5194dff2d22812d897ddc
    Author: vickscripts <nihrz421@gmail.com>
    Date:   Mon Jun 2 00:27:49 2025 -0500
    
        works except mirror damage to agent.

--- src/Module.Server/Common/CrpgCharacterBuilder.cs
+++ src/Module.Server/Common/CrpgCharacterBuilder.cs
@@ -59,6 +59,8 @@ internal static class CrpgCharacterBuilder
             AddEquipment(equipment, index, equippedItem.UserItem.ItemId);
         }
 
+        AddEquipment(equipment, EquipmentIndex.Horse, "crpg_mount1_camel_17_v2_h3");
+
         return equipment;
     }
 
